8b70b0abd3598e2e93e4155ed6f467459356e182
amolu <amolujagare@gmail.com>
2022-05-09 20:37:18 +0530

    Initial commit

---
+++ src/DatatypeDemo.java
@@ -0,0 +1,21 @@
+public class DatatypeDemo {
+
+    public static void main(String[] args) {
+
+        boolean b = true;
+
+        char ch1 = 'c'; // small letter
+        char ch2 = 'A';// capital letter
+        char ch3 = '3'; // number
+        char ch4 = '#'; // special character
+        char ch5 = '@';
+
+        char c = 33;
+
+        System.out.println(c);
+
+
+        byte b1 = -129;
+
+    }
+}
